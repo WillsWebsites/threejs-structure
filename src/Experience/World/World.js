f0c84b74f7587ff075eed264ad32d32224fad225
--replace-all <contactwillswebsites@gmail.com>
2023-11-27 12:33:26 -0700

    Adding models and floor to scene

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three'
 import Experience from '../Experience'
 import Environment from './Environment'
+import Floor from './Floor'
+import Fox from './Fox'
 
 export default class World {
   constructor() {
@@ -8,11 +9,16 @@ export default class World {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    this.mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshStandardMaterial())
-    this.scene.add(this.mesh)
-
     this.resources.on('ready', () => {
+      this.floor = new Floor()
+      this.fox = new Fox()
       this.environment = new Environment()
     })
+  }
+
+  update() {
+    if (this.fox) {
+      this.fox.update()
+    }
   }
 }
