207443947c3b8da32becf96af2c9e4953eb30f08
--replace-all <contactwillswebsites@gmail.com>
2023-11-27 02:24:31 -0700

    Adding environment and others

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,15 +1,18 @@
 import * as THREE from 'three'
 import Experience from '../Experience'
+import Environment from './Environment'
 
 export default class World {
   constructor() {
     this.experience = new Experience()
     this.scene = this.experience.scene
+    this.resources = this.experience.resources
 
-    this.mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ wireframe: true })
-    )
+    this.mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshStandardMaterial())
     this.scene.add(this.mesh)
+
+    this.resources.on('ready', () => {
+      this.environment = new Environment()
+    })
   }
 }
