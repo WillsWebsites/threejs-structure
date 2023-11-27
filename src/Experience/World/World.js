ccfa3e37f77b4942ffdd5b152b5041aae1878588
--replace-all <contactwillswebsites@gmail.com>
2023-11-27 01:37:42 -0700

    first commit

---
+++ src/Experience/World/World.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three'
+import Experience from '../Experience'
+
+export default class World {
+  constructor() {
+    this.experience = new Experience()
+    this.scene = this.experience.scene
+
+    this.mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({ wireframe: true })
+    )
+    this.scene.add(this.mesh)
+  }
+}
